b2d3e82cefd3b97733fb50b725ed05f65190d420
wossenfekadie1993 <wossen.fekadie@gmail.com>
2024-04-27 14:02:05 +0300

    updating about

--- src/components/inputCards/AboutCalculator.tsx
+++ src/components/inputCards/AboutCalculator.tsx
@@ -6,8 +6,8 @@ function AboutTheCalculator() {
   return (
     <div className="bg-secondary/50 rounded-lg p-7 grid gap-2">
       <Label className="font-medium text-xl">things you have to know</Label>
-      <li>li</li>
-      <li>li</li>
+      <p>negative values representing decreasing or loss</p>
+      <p>it estimate best value not the exact </p>
     </div>
   );
 }
