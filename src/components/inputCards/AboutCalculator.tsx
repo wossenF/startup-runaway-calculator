fd1ecdc9a018498f5e3e4ed525a3c82d08bc360f
wossenfekadie1993 <wossen.fekadie@gmail.com>
2024-04-27 13:35:54 +0300

    remove unwanted files

--- src/components/inputCards/AboutCalculator.tsx
+++ src/components/inputCards/AboutCalculator.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { Label } from "../ui/label";
-import { Input } from "../ui/input";
-import useInputStore from "@/store/store";
+
 
 function AboutTheCalculator() {
-  
   return (
     <div className="bg-secondary/50 rounded-lg p-7 grid gap-2">
       <Label className="font-medium text-xl">things you have to know</Label>
       <li>li</li>
       <li>li</li>
-
-      
     </div>
   );
 }
